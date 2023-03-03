f7ca7f26f6465613ebff8c9648f4635c9c87ef24
Vegard46 <31619248+Vegard46@users.noreply.github.com>
2023-03-03 18:16:51 +0100

    Delete src/environments directory

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    POKEMON_API_URL: "https://pokeapi.co/api/v2/pokemon",
-    API_URL: "https://noroff-api-production-156b.up.railway.app/trainers",
-    API_KEY: "1XMN2BaYYgxgu1sRhzWU0DydzNroZmnXNbzGNifZjiCINlNYHTKCNXSMrzhIDHTj"
-};
