a2c5cd50125fee88ffb583d34646312c3b396351
Vegard46 <veg_ander@hotmail.com>
2023-03-03 18:17:22 +0100

    try to remove environments

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable, of, switchMap } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 import { User } from '../models/user.model';
 
 const { API_URL } = environment;
